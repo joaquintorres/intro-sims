e32f7f7fc8f7b2190e212810b197fd0481644404
Joaquin Torres <torres@tandar.cnea.gov.ar>
2023-10-05 14:21:37 -0300

    Add periodic boundary conditions.

--- ising/src/ising.cpp
+++ ising/src/ising.cpp
@@ -1,22 +1,35 @@
 #include <Eigen/Dense>
+#include <Eigen/src/Core/Matrix.h>
 #include <random>
+#include "ising.h"
 #include "types.h"
+
 
 using namespace std;
 
-
+// Generate initial grid with periodic boundary conditions
 Eigen::MatrixXi init_grid(ising_sys_t * isys){
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> distribution(0, 1);
-    Eigen::MatrixXi grid(isys->n, isys->n);
+    Eigen::MatrixXi grid(isys->n + 2*OFFSET_PBC, isys->n + 2*OFFSET_PBC);
     int tmp;
     
-    for (int i=0; i < isys-> n; i++){
-        for (int j=0; j < isys-> n; j++){
+    for (int i=OFFSET_PBC; i <= isys-> n; i++){
+        for (int j=OFFSET_PBC; j <= isys-> n; j++){
             tmp = distribution(gen);
             grid(j, i) = (tmp == 0) ? -1 : 1; // https://eigen.tuxfamily.org/dox-devel/group__TopicStorageOrders.html 
         }
     }
+    periodic_boundary_conditions(grid);
     return grid;
+}
+
+// Copy to second to last row/column from first row/column, do the same with
+// the second column and the last.
+void periodic_boundary_conditions(Eigen::MatrixXi & grid){
+    grid.row(0) = grid.row(grid.rows() - OFFSET_PBC - 1);
+    grid.row(grid.rows() - OFFSET_PBC) = grid.row(OFFSET_PBC);
+    grid.col(0) = grid.col(grid.cols() - OFFSET_PBC - 1);
+    grid.col(grid.cols() - OFFSET_PBC) = grid.col(OFFSET_PBC);
 }
--- ising/src/ising.h
+++ ising/src/ising.h
@@ -3,8 +3,8 @@
 
 #include <Eigen/Dense>
 #include "types.h"
-
+#define OFFSET_PBC 1 // Doesn't actually handle other offsets!
 
 Eigen::MatrixXi init_grid(ising_sys_t * isys);
-
+void periodic_boundary_conditions(Eigen:: MatrixXi & grid);
 #endif
